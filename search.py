9aec48d161106dc11e0313eea52a553b3ee4099f
silviatommaso <silvia.tommaso18112002@gmail.com>
2026-01-10 13:17:10 +0100

    Xpath research

--- search.py
+++ search.py
@@ -28,4 +28,14 @@ class Search:
                 }
             }
         )
+    
+    def create_index(self):
+        self.es.indices.delete(index='hwk5_dataIng', ignore_unavailable=True)
+        self.es.indices.create(index='hwk5_dataIng')
 
+
+    def insert_document(self, document):
+        return self.es.index(index='hwk5_dataIng', body=document)
+    
+
+    #authors: "ltx_title ltx_titledocument" --- "ltx_personname", "ltx_document ltx_authors_1line"--> in questo caso restituire il contenuto intero del nipote <p>, ltx_abstract,  
